586d4ae8dd6037b0e52b82e6865f6e39924f7368
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2026-02-16 02:19:40 +0700

    test 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ application {
     mainClass = "org.delcom.ApplicationKt"
 }
 
+
 dependencies {
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
